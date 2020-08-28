bdf7ff920fa6a9c1fe9b3e6b76bda03e4cb68402
Hank Cheng <hcheng10@gmail.com>
2020-08-28 16:50:56 -0700

    implement digital ocean space interface V2

--- client/src/digitalocean/index.js
+++ client/src/digitalocean/index.js
@@ -1,19 +1,19 @@
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 
-/**
- * Digital Ocean Spaces Connection
- */
+// /**
+//  * Digital Ocean Spaces Connection
+//  */
 
-const spacesEndpoint = new AWS.Endpoint('sfo2.digitaloceanspaces.com');
-const s3 = new AWS.S3({
-    endpoint: spacesEndpoint,
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.ACCESS_SECRET_KEY
-});
+// const spacesEndpoint = new AWS.Endpoint('sfo2.digitaloceanspaces.com');
+// const s3 = new AWS.S3({
+//     endpoint: spacesEndpoint,
+//     accessKeyId: process.env.ACCESS_KEY_ID,
+//     secretAccessKey: process.env.ACCESS_SECRET_KEY
+// });
 
-const doConfig = {
-    digitalOceanSpaces: 'https://myBucketName.nyc3.digitaloceanspaces.com/',
-    bucketName: 'bucket_1'
-};
+// const doConfig = {
+//     digitalOceanSpaces: 'https://myBucketName.nyc3.digitaloceanspaces.com/',
+//     bucketName: 'bucket_1'
+// };
 
-export { doConfig, s3 as default };
+// export { doConfig, s3 as default };
